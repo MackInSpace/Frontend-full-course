e702244fe6ec4ba84839a6877ec4b42cba9ab471
MackInSpace <amackey@lssu.edu>
2025-04-24 17:25:29 -0600

    started calendar section

--- reactjs-tutorial/src/components/Calender.jsx
+++ reactjs-tutorial/src/components/Calender.jsx
@@ -1,4 +1,10 @@
 export default function Calender() {
+    const { lifeExpectancy, data } = props
+    const yearsArr = [...Array(lifeExpectancy).keys()]
+    const weeksArr = [...Array(52).keys()]
+    const weekNum = lifeExpectancy * 52 - parseInt(data['weeks'])
+    const finalWeek = lifeExpectancy * 52
+
     return (
         <div></div>
     )
