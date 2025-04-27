0058915eaadc03cca02e95dfa9a364ed82bce754
MackInSpace <amackey@lssu.edu>
2025-04-27 08:47:39 -0600

    finished grid

--- reactjs-tutorial/src/components/Calender.jsx
+++ reactjs-tutorial/src/components/Calender.jsx
@@ -6,6 +6,15 @@ export default function Calender() {
     const finalWeek = lifeExpectancy * 52
 
     return (
-        <div></div>
+        <section>
+            <p><i>Each square of dots represents 52 weeks / 1 year of your life.</i></p>
+            <div className="dozen-grid">
+                {yearsArr.map(year, yearIndex => {
+                    return (
+                        <DozenDisplay key={yearIndex} weeksArr={weeksArr} weekNum={weekNum} finalWeek={finalWeek} yearIndex={yearIndex} />
+                    )
+                })}
+            </div>
+        </section> 
     )
 }
