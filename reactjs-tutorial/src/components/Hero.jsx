d825d95f5cf8c26df15c3e1cb1ed9bb357abe368
MackInSpace <amackey@lssu.edu>
2025-05-12 13:16:24 -0600

    hero done

--- reactjs-tutorial/src/components/Hero.jsx
+++ reactjs-tutorial/src/components/Hero.jsx
@@ -1,5 +1,32 @@
-export default function Hero() {
+export default function Hero(props) {
+    const { name, resetData, data, percentage, handleToggleModal } = props
+
     return (
-        <div></div>
+        <section id="hero">
+            <h3 className="text-large">
+                {name}, you have {data.weeks} weeks left. Make them count!
+            </h3>
+            <div className="btns-container">
+                <button onClick={handleToggleModal}>Not {name}?</button>
+                <button onClick={() => {
+                    navigator.clipboard.writeText('https://github.com/MackInSpace/Frontend-full-course/tree/main/reactjs-tutorial')
+                    alert('Copied :)')
+                }} className="link-button">Copy Link</button>\
+                <button onClick={resetData} className="link-button">Reset data</button>
+            </div>
+            <div className="progress-bar">
+                <div style={{ width: `${percentage}` }}>
+                    <div>
+                        <i className="fa-solid fa-baby" />
+                        <h6 className="bar-label">Birth</h6>
+                    </div>
+                    <h6>{percentage}</h6>
+                </div>
+                <div>
+                    <h6 className="bar-label">Death</h6>
+                    <i className="fa-solid fa-skull" />
+                </div>
+            </div>
+        </section>
     )
 }
