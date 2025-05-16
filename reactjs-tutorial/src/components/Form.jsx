e75392a0b712cf2a8eef25e9ea986ff9b3729673
MackInSpace <amackey@lssu.edu>
2025-05-16 16:47:53 -0600

    completed form and thus the project

--- reactjs-tutorial/src/components/Form.jsx
+++ reactjs-tutorial/src/components/Form.jsx
@@ -39,6 +39,52 @@ export default function Form(props) {
                     setName(e.target.value)
                 }} type='text' required />
             </div>
+            <div>
+                <label>Date of Birth</label>
+                <div className='bday'>
+                    <select value={day}
+                        onChange= {(e) => {
+                            setDay(e.target.value)
+                        }}>
+                        {days.map((d) => {
+                            return (
+                                <option key={d} value={d}>{d}</option>
+                            )
+                        })}
+                    </select>
+                    <select value={month}
+                        onChange={(e) => {
+                            setMonth(e.target.value)
+                        }}>
+                        {months.map((m, i) => {
+                            return (
+                                <option key={i} value={i + 1}>{m}</option>
+                            )
+                        })}
+                    </select>
+                    <select value={year}
+                        onChange={(e) => {
+                            setYear(e.target.value)
+                        }}>
+                        {years.map((y) => {
+                            return (
+                                <option key={y} value={y}>{y}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+            </div>
+            <div>
+                <label>Life Expectancy (years)</label>
+                <input value={lifeExpectancy} onChange={(e) => {
+                    setLifeExpectancy(e.target.value)
+                }} type='number' max={120} required />
+            </div>
+            <button onClick={() => {
+                handleCloseModal(name, `${year}-${month}-${day}`, lifeExpectancy)
+            }}>
+                Save
+            </button>
         </section>
     )
 }
