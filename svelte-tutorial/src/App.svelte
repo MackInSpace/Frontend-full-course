45022230c4a90534ebf274cf9e7bc5c19d138e33
MackInSpace <amackey@lssu.edu>
2025-05-26 12:26:50 -0600

    started app and hero

---
+++ svelte-tutorial/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Layout from "./lib/layouts/Layout.svelte";
+    import Summary from "./lib/Summary.svelte";
+    import Hero from "./lib/Hero.svelte";
+    import Clocks from "./lib/Clocks.svelte";
+    import Calendar from "./lib/Calendar.svelte";
+</script>
+
+<Layout>
+    <Hero />
+    <Clocks />
+    <Calendar />
+    <Summary />
+ </Layout>
