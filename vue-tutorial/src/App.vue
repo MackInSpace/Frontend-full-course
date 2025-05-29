c37be506cb55c895b82305545c59e4403aed5d46
MackInSpace <amackey@lssu.edu>
2025-05-29 14:26:20 -0600

    added components to project

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,30 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { computed, onMounted, ref, watchEffect } from 'vue'
+  import Calendar from './components/Calendar.vue';
+  import Clocks from './components/Clocks.vue';
+  import Hero from './components/Hero.vue';
+  import Layout from './components/Layout.vue';
+  import Summary from './components/Summary.vue';
+  import Form from './components/Form.vue';
+
+  import { calculateTimeLeft, getLifePercentageLived } from './utils';
+  import Protal from './components/Portal.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
