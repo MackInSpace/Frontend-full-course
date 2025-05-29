698ab8865915dde87b64a28ecf075e86f881ff02
MackInSpace <amackey@lssu.edu>
2025-05-29 14:33:01 -0600

    working on app

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -8,7 +8,40 @@
   import Form from './components/Form.vue';
 
   import { calculateTimeLeft, getLifePercentageLived } from './utils';
-  import Protal from './components/Portal.vue';
+  import Portal from './components/Portal.vue';
+
+  const defaultBD = '1988-01-18';
+  const defaultLE = 80;
+  const birthDate = ref(defaultBD);
+  const lifeExpectancy = ref(defaultLE);
+  const name = ref('adam');
+  const data = ref(calculateTimeLeft(defaultBD, defaultLE));
+  console.log(data);
+  let percentage = computed(() => getLifePercentageLived(birthDate.value, lifeExpectancy.value))
+
+  const showModal = ref(false)
+  function handleToggleModal() {
+    showModal.value = !showModal.value
+  }
+
+  function handleUpdateData(n, b, e) {
+    if (!n || !b || !e) {
+      return;
+    }
+
+    //save new data to local storage browser database
+    localStorage.setItem('formData', JSON.stringify({
+      name: n,
+      birthDate: b,
+      lifeExpectancy: e
+    }))
+
+    name.value = n
+    birthDate.value = b
+    lifeExpectancy.value = parseInt(e)
+    data.value = calculateTimeLeft(b, parseInt(e))
+    showModal.value = false
+  }
 
 </script>
 
