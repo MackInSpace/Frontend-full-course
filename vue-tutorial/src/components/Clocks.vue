321e7c1665cd27bc885bac424d40703ebd7dddac
MackInSpace <amackey@lssu.edu>
2025-05-31 15:29:33 -0600

    clock done

--- vue-tutorial/src/components/Clocks.vue
+++ vue-tutorial/src/components/Clocks.vue
@@ -1,9 +1,29 @@
 <script>
+    import { getTimePercentage } from '../utils/'
 
+    defineProps({
+        data: Object
+    })
+    let snapshot = getTimePercentage()
+    console.log(snapshot)
 </script>
 
 <template>
-
+    <section>
+        <p><i>Time remaining in different units.</i></p>
+        <div class="clocks-grid">
+            <div class="card clock-card" v-for="(clock, clockIndex) in Object.keys(data.value)" :key="index">
+                <div class="circle" :style="{ rotate: snapshot[clock] + 'deg'}">
+                    <div class="ticker" :class="{[clock]: true}">
+                    </div>
+                </div>
+                <div>
+                    <h5>{{ data.value[clock] }}</h5>
+                    <p>{{ clock }}</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
