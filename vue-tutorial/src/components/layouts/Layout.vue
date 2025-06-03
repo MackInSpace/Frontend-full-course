4f0234f111b1740163acc00a04640573da5408c7
MackInSpace <amackey@lssu.edu>
2025-06-03 11:50:07 -0600

    layout done

---
+++ vue-tutorial/src/components/layouts/Layout.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <h1 class="text-medium text-gradient">Unalive</h1>
+    </header>
+    <main>
+        <slot />
+    </main>
+    <footer>
+        <small>Created By:</small>
+        <a href="/" target="blank">
+            <img src="https://github.com/MackInSpace.png" alt="MackInSpace" />
+            <p>@MackInSpace</p>
+            <i class="fa-brands fa-github"></i>
+        </a>
+    </footer>
+</template>
+
+<style scoped></style>
